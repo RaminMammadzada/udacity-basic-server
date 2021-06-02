77ae09206a8e252d91ab5afaddc6938c07eda78f
Ramin Mammadzada <remki-design@hotmail.com>
2021-06-02 20:27:52 +0300

    Add REST api.

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "remki",
+    "password": "remki1299",
+    "database": "udacityfs1",
+    "host": "database-2.cspdxhpfdw5f.eu-central-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "eu-central-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udacitycloud2",
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
